0a93ae30ea39bb198c6d6609fe33c5c2a90a66d3
idben <idben.chen@gmail.com>
2024-08-06 21:43:43 +0800

    組件化，import 使用

---
+++ component/Loading.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+
+const Loading = ({time, path}) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => url !== Router.asPath && setLoading(true);
+
+    const waitting = (t) => {
+      return new Promise(resolve => {
+        setTimeout(()=>{
+          resolve();
+        },t);
+      });
+    }
+
+    const handleComplete = async (url) => {
+      if (url === Router.asPath) {
+        if(time && time > 0){
+          await waitting(time);
+        }
+        setLoading(false);
+      }
+    };
+
+    Router.events.on("routeChangeStart", handleStart);
+    Router.events.on("routeChangeComplete", handleComplete);
+    Router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleStart);
+      Router.events.off("routeChangeComplete", handleComplete);
+      Router.events.off("routeChangeError", handleComplete);
+    };
+  }, [time]);
+
+  return loading ? (
+    <div className="loading">
+      <Player
+        autoplay
+        loop
+        src={path}
+        style={{ height: "300px", width: "300px" }}
+      />
+    </div>
+  ) : null;
+};
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.sass";
-import Router from "next/router";
-import { useState, useEffect } from "react";
-import { Player, Controls } from "@lottiefiles/react-lottie-player";
-
-const Loading = ({time, path}) => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const handleStart = (url) => url !== Router.asPath && setLoading(true);
-
-    const waitting = (t) => {
-      return new Promise(resolve => {
-        setTimeout(()=>{
-          resolve();
-        },t);
-      });
-    }
-
-    const handleComplete = async (url) => {
-      if (url === Router.asPath) {
-        if(time && time > 0){
-          await waitting(time);
-        }
-        setLoading(false);
-      }
-    };
-
-    Router.events.on("routeChangeStart", handleStart);
-    Router.events.on("routeChangeComplete", handleComplete);
-    Router.events.on("routeChangeError", handleComplete);
-
-    return () => {
-      Router.events.off("routeChangeStart", handleStart);
-      Router.events.off("routeChangeComplete", handleComplete);
-      Router.events.off("routeChangeError", handleComplete);
-    };
-  }, [time]);
-
-  return loading ? (
-    <div className="loading">
-      <Player
-        autoplay
-        loop
-        src={path}
-        style={{ height: "300px", width: "300px" }}
-      />
-    </div>
-  ) : null;
-};
+import Loading from "@/component/Loading";
 
 export default function App({ Component, pageProps }) {
   return (
